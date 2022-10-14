39e568ff00ebe9cf6e1f92856aab9230b5edcb38
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-10-14 18:09:06 +0300

    Initial Commit

---
+++ Eco Motion/Eco_MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_MotionApp.swift
+//  Eco Motion
+//
+//  Created by Mert Tecimen on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Eco_MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
